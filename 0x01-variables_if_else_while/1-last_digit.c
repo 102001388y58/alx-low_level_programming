66ffe4257e18f737245e53e5335415ce2416cecb
102001388y58 <portiafiyado@gmail.com>
2022-12-09 10:14:41 -0800

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,14 +6,14 @@
  * main - Entry
  * Return: Always 0
  */
-/*betty style doc for function main goes there */
+/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
 	int ld_n; /* last digit of n */
 
 	srand(time(0));
-	n = rand() -RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	ld_n = n % 10;
 	/* your code goes there */
 	if (ld_n > 5)
