8a0e43cda5a84e7a3bc3718370fa7b711f4df57e
102001388y58 <portiafiyado@gmail.com>
2022-12-09 03:00:21 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return: Always 0 
+ * Return: Always 0
  */
-int maid(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
