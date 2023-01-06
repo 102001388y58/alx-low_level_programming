182fca99c6084ee235f5a623b8397767d4050e39
102001388y58 <portiafiyado@gmail.com>
2023-01-06 11:41:07 -0800

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the number of arguments
+ * @argc:The size of argv array
+ * @argv: an array of size arg c
+ * Return: Always 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
