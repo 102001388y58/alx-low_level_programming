de67c0534863e24d009e647888e872bd9507b4a1
102001388y58 <portiafiyado@gmail.com>
2023-01-16 13:41:31 -0800

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,7 +19,7 @@ struct dog
 typedef struct dog dog_t;
 
 /* function prototypes */
-void init_dog(struct dog *d, char *name, float age,char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 int _strlen(char *str);
 char *_strcopy(char *dest, char *src);
