3a46af24d283f5cacab7832198e431ef7b89dd32
102001388y58 <portiafiyado@gmail.com>
2023-01-16 13:24:48 -0800

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+/* new type of struct dog */
+typedef struct dog dog_t;
+
+/* function prototypes */
+void init_dog(struct dog *d, char *name, float age,char *owner);
+void print_dog(struct dog *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
